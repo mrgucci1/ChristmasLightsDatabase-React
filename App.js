94e6a06b852d00a5bdec8306ceb650deb5578ee5
mrgucci1 <dalquist.keaton221@gmail.com>
2021-11-09 22:12:17 -0600

    finally figured out firebase kinda for react-native, able to access my firestore db

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {db, auth} from './Fire'
 export default function App() {
+  const[name, setName] = useState('');
+  const getName = () => {
+    db.collection('user').doc("01").get().then(doc => {
+      setName(doc.data().name);
+    })
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! {getName()}{name}</Text>
       <StatusBar style="auto" />
     </View>
   );
